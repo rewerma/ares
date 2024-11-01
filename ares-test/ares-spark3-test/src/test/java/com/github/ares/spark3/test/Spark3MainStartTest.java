e5fb9438f86816eca8610df85a7ff32dd0b9dc95
mcy <rewrma@163.com>
2024-11-01 22:08:34 +0800

    optimize code

--- ares-test/ares-spark3-test/src/test/java/com/github/ares/spark3/test/Spark3MainStartTest.java
+++ ares-test/ares-spark3-test/src/test/java/com/github/ares/spark3/test/Spark3MainStartTest.java
@@ -2,8 +2,10 @@ package com.github.ares.spark3.test;
 
 import com.github.ares.spark.starter.AresSparkStarter;
 import com.github.ares.test.spark.Utils;
+import org.junit.Ignore;
 import org.junit.Test;
 
+@Ignore
 public class Spark3MainStartTest {
     @Test
     public void testMain() {
