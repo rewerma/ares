ed4dfdfc0d5e0abbd8e84af831f3e1b7307c04b0
machey <rewerma@163.com>
2024-12-01 22:04:15 +0800

    optimize code

---
+++ ares-admin/ares-web/src/main/java/com/github/ares/web/service/CallbackHandler.java
@@ -0,0 +1,65 @@
+package com.github.ares.web.service;
+
+import com.github.ares.common.enums.StatusType;
+import com.github.ares.common.utils.JsonUtils;
+import com.github.ares.web.entity.TaskDefinition;
+import com.github.ares.web.entity.TaskInstance;
+import com.github.ares.worker.Callback;
+import com.github.ares.worker.model.TaskResponse;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+public class CallbackHandler implements Callback {
+    private final TaskDefinition taskDefinition;
+    private final TaskInstance taskInstance;
+
+    public CallbackHandler(TaskDefinition taskDefinition, TaskInstance taskInstance) {
+        this.taskDefinition = taskDefinition;
+        this.taskInstance = taskInstance;
+    }
+
+    @Override
+    public void running(String logPath) {
+        taskInstance.setLogPath(logPath);
+        taskInstance.setStatus(StatusType.RUNNING.getValue());
+        taskInstance.update("logPath", "status");
+    }
+
+    @Override
+    public void completed(TaskResponse taskResponse) {
+        if (taskResponse.getStatus() == StatusType.SUCCESS) {
+            Map<String, Object> outputParams = taskResponse.getOutputParams();
+            if (taskDefinition.getOutParams() != null && outputParams != null) {
+                taskInstance.setOutParams(JsonUtils.toJsonString(outputParams));
+            }
+            List<Map<String, Object>> resultJson = taskResponse.getLastResult();
+            if (resultJson != null) {
+                String result = JsonUtils.toJsonString(resultJson);
+                taskInstance.setExeResult(result);
+            }
+            taskInstance.setStatus(StatusType.SUCCESS.getValue());
+            taskInstance.setEndTime(LocalDateTime.now());
+            taskInstance.setLogPath(taskResponse.getLogPath());
+            taskInstance.update("status", "outParams", "endTime", "exeResult", "logPath");
+        } else if (taskResponse.getStatus() == StatusType.FAILED) {
+            taskInstance.setStatus(StatusType.FAILED.getValue());
+            taskInstance.setEndTime(LocalDateTime.now());
+            taskInstance.setExeResult(taskResponse.getErrorMessage());
+            taskInstance.update("status", "endTime", "exeResult");
+        } else if (taskResponse.getStatus() == StatusType.STOPPED) {
+            taskInstance.setStatus(StatusType.STOPPED.getValue());
+            taskInstance.setEndTime(LocalDateTime.now());
+            taskInstance.update("status", "endTime");
+        } else {
+            log.error("task execution type error: {}", taskResponse.getStatus());
+            taskInstance.setStatus(StatusType.FAILED.getValue());
+            taskInstance.setEndTime(LocalDateTime.now());
+            taskInstance.setExeResult("task execution callback type: " + taskResponse.getStatus().name());
+            taskInstance.update("status", "endTime", "exeResult");
+        }
+    }
+}
--- ares-admin/ares-web/src/main/java/com/github/ares/web/service/TaskExecutionService.java
+++ ares-admin/ares-web/src/main/java/com/github/ares/web/service/TaskExecutionService.java
@@ -9,10 +9,8 @@ import com.github.ares.web.entity.TaskInstance;
 import com.github.ares.web.utils.CodeGenerator;
 import com.github.ares.web.utils.NetUtils;
 import com.github.ares.web.utils.ServiceException;
-import com.github.ares.worker.Callback;
 import com.github.ares.worker.TaskWorker;
 import com.github.ares.worker.model.TaskContext;
-import com.github.ares.worker.model.TaskResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +18,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -67,57 +64,6 @@ public class TaskExecutionService {
         taskWorker.start(taskContext);
 
         return taskInstance.getId();
-    }
-
-    private static class CallbackHandler implements Callback {
-        private final TaskDefinition taskDefinition;
-        private final TaskInstance taskInstance;
-
-        public CallbackHandler(TaskDefinition taskDefinition, TaskInstance taskInstance) {
-            this.taskDefinition = taskDefinition;
-            this.taskInstance = taskInstance;
-        }
-
-        @Override
-        public void running(String logPath) {
-            taskInstance.setLogPath(logPath);
-            taskInstance.setStatus(StatusType.RUNNING.getValue());
-            taskInstance.update("logPath", "status");
-        }
-
-        @Override
-        public void completed(TaskResponse taskResponse) {
-            if (taskResponse.getStatus() == StatusType.SUCCESS) {
-                Map<String, Object> outputParams = taskResponse.getOutputParams();
-                if (taskDefinition.getOutParams() != null && outputParams != null) {
-                    taskInstance.setOutParams(JsonUtils.toJsonString(outputParams));
-                }
-                List<Map<String, Object>> resultJson = taskResponse.getLastResult();
-                if (resultJson != null) {
-                    String result = JsonUtils.toJsonString(resultJson);
-                    taskInstance.setExeResult(result);
-                }
-                taskInstance.setStatus(StatusType.SUCCESS.getValue());
-                taskInstance.setEndTime(LocalDateTime.now());
-                taskInstance.setLogPath(taskResponse.getLogPath());
-                taskInstance.update("status", "outParams", "endTime", "exeResult", "logPath");
-            } else if (taskResponse.getStatus() == StatusType.FAILED) {
-                taskInstance.setStatus(StatusType.FAILED.getValue());
-                taskInstance.setEndTime(LocalDateTime.now());
-                taskInstance.setExeResult(taskResponse.getErrorMessage());
-                taskInstance.update("status", "endTime", "exeResult");
-            } else if (taskResponse.getStatus() == StatusType.STOPPED) {
-                taskInstance.setStatus(StatusType.STOPPED.getValue());
-                taskInstance.setEndTime(LocalDateTime.now());
-                taskInstance.update("status", "endTime");
-            } else {
-                log.error("task execution type error: {}", taskResponse.getStatus());
-                taskInstance.setStatus(StatusType.FAILED.getValue());
-                taskInstance.setEndTime(LocalDateTime.now());
-                taskInstance.setExeResult("task execution callback type: " + taskResponse.getStatus().name());
-                taskInstance.update("status", "endTime", "exeResult");
-            }
-        }
     }
 
     private String handleDatasource(String dsCode, String taskContent) {
